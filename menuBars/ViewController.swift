0b8eda4354a30e252fa6bafe4edcd20081529fbb
West Kraemer <WestKraemer@gmail.com>
2017-01-12 11:30:40 -0500

    setting menubars and tool bars with icons and flexible space bars

--- menuBars/ViewController.swift
+++ menuBars/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func camera(_ sender: Any) {
+        print("camera")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
